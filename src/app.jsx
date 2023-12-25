422b81f35d175c970e1916bd7ffedb85f9038a48
Shadowjian <karakalel.adorable@gmail.com>
2023-12-25 22:20:14 +0800

    revert to browserrouter

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import About from "./pages/About"
 import Home from "./pages/Home"
-import Vans from "./pages/Vans"
 
 import "./server"
 import Nav from "./components/nav"
 import { useEffect, useState } from "react"
 import VanDetail from "./pages/VanDetail"
+import Vans from "./pages/vans"
 
 export default function App() {
   const [vans, setVans] = useState([])
@@ -17,15 +17,8 @@ export default function App() {
       .then((data) => setVans(data.vans))
   }, [])
   return (
-    <HashRouter>
+    <BrowserRouter>
       <header>
-        {/* <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav> */}
         <Nav />
       </header>
       <Routes>
@@ -34,6 +27,6 @@ export default function App() {
         <Route path="/vans" element={<Vans vans={vans} />} />
         <Route path="/vans/:id" element={<VanDetail vans={vans} />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
